ba53a4b2486bd82e277f507269f5fe3c90bbcfa1
Michael Carey <contact@michaelcarey.com.au>
2025-03-26 20:45:45 +1000

    sync up with tutorial repo

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -2,10 +2,10 @@ import { Footer } from "./footer";
 import { Header } from "./header";
 
 type Props = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
-const MarketingLayout = ({ children }: Props ) => {
+const MarketingLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
